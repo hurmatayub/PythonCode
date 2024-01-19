86342a280eda71cb5779770fe92828c85e242113
hurmatayub <156796678+hurmatayub@users.noreply.github.com>
2024-01-19 21:17:34 +0500

    Add files via upload

---
+++ HurmatAyub.py
@@ -0,0 +1,15 @@
+a = int(input('enter a number 1:'))
+b = int(input('enter a number 2:'))
+c = int(input('enter 1 for addition, enter 2 for subtraction, enter 3 for multiplication, enter 4 for division'))
+
+if(c==1):
+    print("Answer is : ", (a+b))
+
+if(c==2):
+    print("Answer is : ", (a-b))
+
+if(c==3):
+    print("Answer is : ", (a*b))
+
+if(c==4):
+    print("Answer is : ", (a/b))    
